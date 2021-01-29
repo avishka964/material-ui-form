de2bca5f1bfcfdf9f07c433c42f53f881421f677
avishka964 <avishkakapuruge@gmail.com>
2021-01-29 23:20:49 +0530

    validations update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from "react"
-import { Router, Switch, Route } from "react-router-dom"
 import UserForm from "./screens/UserForm"
-import Submit from "./screens/Submit"
-import history from "./components/history"
 
 function App() {
   return (
-    <Router history={history}>
-      <Switch>
-        <Route path="/" component={UserForm} exact />
-        <Route path="/Submit" component={Submit} />
-      </Switch>
-    </Router>
+    <div>
+      <UserForm />
+    </div>
   )
 }
 
--- src/screens/UserForm.js
+++ src/screens/UserForm.js
@@ -1,7 +1,6 @@
-import React, { useState } from "react"
+import React from "react"
 import {
   Grid,
-  TextField,
   makeStyles,
   Card,
   CardContent,
@@ -12,14 +11,11 @@ import {
   Button,
   CardHeader,
   FormControl,
-  OutlinedInput,
-  InputAdornment,
-  IconButton,
 } from "@material-ui/core"
 
-import Visibility from "@material-ui/icons/Visibility"
-import VisibilityOff from "@material-ui/icons/VisibilityOff"
-import history from "../components/history"
+import { Formik, Form, Field } from "formik"
+import * as Yup from "yup"
+import { TextField } from "formik-material-ui"
 
 const useStyle = makeStyles((theme) => ({
   padding: {
@@ -39,7 +35,6 @@ const initialValues = {
   country: "",
   email: "",
   password: "",
-  showPassword: false,
 }
 
 const options = [
@@ -50,31 +45,36 @@ const options = [
   { label: "Quality Assurance Tester", value: "quality_assurance_tester" },
 ]
 
+//password validation
+const lowercaseRegEx = /(?=.*[a-z])/
+const uppercaseRegEx = /(?=.*[A-Z])/
+const numericRegEx = /(?=.*[0-9])/
+const lengthRegEx = /(?=.{6,})/
+
+//validation schema
+let validationSchema = Yup.object().shape({
+  firstName: Yup.string().required("Required"),
+  lastName: Yup.string().required("Required"),
+  email: Yup.string().email("Invalid email").required("Required"),
+  password: Yup.string()
+    .matches(
+      lowercaseRegEx,
+      "Must contain one lowercase alphabetical character!"
+    )
+    .matches(
+      uppercaseRegEx,
+      "Must contain one uppercase alphabetical character!"
+    )
+    .matches(numericRegEx, "Must contain one numeric character!")
+    .matches(lengthRegEx, "Must contain 6 characters!")
+    .required("Required!"),
+})
+
 const UserForm = () => {
-  const [values, setValues] = useState(initialValues)
-
-  const handleClickShowPassword = () => {
-    setValues({ ...values, showPassword: !values.showPassword })
-  }
-
-  const handleMouseDownPassword = (event) => {
-    event.preventDefault()
-  }
   const classes = useStyle()
 
-  //   Handle Field Change
-  const handleChange = (e) => {
-    const { name, value } = e.target
-    setValues({
-      ...values,
-      [name]: value,
-    })
-  }
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
+  const onSubmit = (values) => {
     console.log(values)
-    history.push("/Submit")
   }
 
   return (
@@ -82,126 +82,115 @@ const UserForm = () => {
       <Grid item md={6}>
         <Card className={classes.padding}>
           <CardHeader title="REGISTER FORM"></CardHeader>
-          <form onSubmit={handleSubmit}>
-            <CardContent>
-              <Grid item container spacing={1} justify="center">
-                <Grid item xs={12} sm={6} md={6}>
-                  <TextField
-                    label="First Name"
-                    variant="outlined"
-                    fullWidth
-                    name="firstName"
-                    required
-                    helperText="First name is required."
-                    onChange={handleChange}
-                  />
-                </Grid>
-                <Grid item xs={12} sm={6} md={6}>
-                  <TextField
-                    label="Last Name"
-                    variant="outlined"
-                    fullWidth
-                    name="lastName"
-                    required
-                    helperText="Last name is required."
-                    onChange={handleChange}
-                  />
-                </Grid>
+          <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={onSubmit}>
+            {({ dirty, isValid, values, handleChange, handleBlur }) => {
+              return (
+                <Form>
+                  <CardContent>
+                    <Grid item container spacing={1} justify="center">
+                      <Grid item xs={12} sm={6} md={6}>
+                        <Field
+                          label="First Name"
+                          variant="outlined"
+                          fullWidth
+                          name="firstName"
+                          value={values.firstName}
+                          component={TextField}
+                        />
+                      </Grid>
+                      <Grid item xs={12} sm={6} md={6}>
+                        <Field
+                          label="Last Name"
+                          variant="outlined"
+                          fullWidth
+                          name="lastName"
+                          value={values.lastName}
+                          component={TextField}
+                        />
+                      </Grid>
 
-                <Grid item xs={12} sm={6} md={12}>
-                  <FormControl fullWidth variant="outlined">
-                    <InputLabel id="demo-simple-select-outlined-label">
-                      Occupation
-                    </InputLabel>
-                    <Select
-                      labelId="demo-simple-select-outlined-label"
-                      id="demo-simple-select-outlined"
-                      label="Occupation"
-                      value={values.occupation}
-                      onChange={handleChange}
-                      name="occupation">
-                      <MenuItem>None</MenuItem>
-                      {options.map((item) => (
-                        <MenuItem key={item.value} value={item.value}>
-                          {item.label}
-                        </MenuItem>
-                      ))}
-                    </Select>
-                  </FormControl>
-                </Grid>
-                <Grid item xs={12} sm={6} md={6}>
-                  <TextField
-                    label="City"
-                    variant="outlined"
-                    fullWidth
-                    name="city"
-                    value={values.city}
-                    onChange={handleChange}
-                  />
-                </Grid>
-                <Grid item xs={12} sm={6} md={6}>
-                  <TextField
-                    label="Country"
-                    variant="outlined"
-                    fullWidth
-                    name="country"
-                    value={values.country}
-                    onChange={handleChange}
-                  />
-                </Grid>
-                <Grid item xs={12} sm={6} md={6}>
-                  <TextField
-                    label="Email"
-                    variant="outlined"
-                    fullWidth
-                    name="email"
-                    value={values.email}
-                    onChange={handleChange}
-                  />
-                </Grid>
-                <Grid item xs={12} sm={6} md={6}>
-                  <FormControl fullWidth variant="outlined">
-                    <InputLabel htmlFor="outlined-adornment-password">
-                      Password
-                    </InputLabel>
-                    <OutlinedInput
-                      id="outlined-adornment-password"
-                      type={values.showPassword ? "text" : "password"}
-                      value={values.password}
-                      onChange={handleChange}
-                      name="password"
-                      endAdornment={
-                        <InputAdornment position="end">
-                          <IconButton
-                            aria-label="toggle password visibility"
-                            onClick={handleClickShowPassword}
-                            onMouseDown={handleMouseDownPassword}
-                            edge="end">
-                            {values.showPassword ? (
-                              <Visibility />
-                            ) : (
-                              <VisibilityOff />
-                            )}
-                          </IconButton>
-                        </InputAdornment>
-                      }
-                      labelWidth={70}
-                    />
-                  </FormControl>
-                </Grid>
-              </Grid>
-            </CardContent>
-            <CardActions>
-              <Button
-                variant="contained"
-                color="primary"
-                type="Submit"
-                className={classes.button}
-                onClick={handleSubmit}>
-                REGISTER
-              </Button>
-            </CardActions>
-          </form>
+                      <Grid item xs={12} sm={6} md={12}>
+                        <FormControl fullWidth variant="outlined">
+                          <InputLabel id="demo-simple-select-outlined-label">
+                            Occupation
+                          </InputLabel>
+                          <Select
+                            labelId="demo-simple-select-outlined-label"
+                            id="demo-simple-select-outlined"
+                            label="Occupation"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.occupation}
+                            name="occupation">
+                            <MenuItem>None</MenuItem>
+                            {options.map((item) => (
+                              <MenuItem key={item.value} value={item.value}>
+                                {item.label}
+                              </MenuItem>
+                            ))}
+                          </Select>
+                        </FormControl>
+                      </Grid>
+                      <Grid item xs={12} sm={6} md={6}>
+                        <Field
+                          label="City"
+                          variant="outlined"
+                          fullWidth
+                          name="city"
+                          value={values.city}
+                          component={TextField}
+                        />
+                      </Grid>
+                      <Grid item xs={12} sm={6} md={6}>
+                        <Field
+                          label="Country"
+                          variant="outlined"
+                          fullWidth
+                          name="country"
+                          value={values.country}
+                          component={TextField}
+                        />
+                      </Grid>
+                      <Grid item xs={12} sm={6} md={6}>
+                        <Field
+                          label="Email"
+                          variant="outlined"
+                          fullWidth
+                          name="email"
+                          value={values.email}
+                          component={TextField}
+                        />
+                      </Grid>
+                      <Grid item xs={12} sm={6} md={6}>
+                        <Field
+                          label="Password"
+                          variant="outlined"
+                          fullWidth
+                          name="password"
+                          value={values.password}
+                          type="password"
+                          component={TextField}
+                        />
+                      </Grid>
+                    </Grid>
+                  </CardContent>
+                  <CardActions>
+                    <Button
+                      disabled={!dirty || !isValid}
+                      variant="contained"
+                      color="primary"
+                      type="Submit"
+                      className={classes.button}>
+                      REGISTER
+                    </Button>
+                  </CardActions>
+                </Form>
+              )
+            }}
+          </Formik>
         </Card>
       </Grid>
     </Grid>
